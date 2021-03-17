cc8f94d32f945915bf531b7524710c66ee1459c2
Griffin T <a12345678910ster+git@gmail.com>
2021-03-17 17:30:24 +0800

    format

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const $ = document.querySelector.bind( document );
+const $ = document.querySelector.bind(document);
 
 const game = {
     starting: false,
@@ -17,40 +17,40 @@ const game = {
 const RED_COLOR = 'red';// 'hsla(0, 85%, 55%, 1)';
 const GREEN_COLOR = 'limegreen';//'hsl(116, 85%, 55%)';
 
-const LEADERBOARD_TEMPLATE = $( "#leaderboard-template" ).innerHTML;
-const HISTORY_TEMPLATE = $( "#history-template" ).innerHTML;
+const LEADERBOARD_TEMPLATE = $("#leaderboard-template").innerHTML;
+const HISTORY_TEMPLATE = $("#history-template").innerHTML;
 
 
-let target_el = $( '#target' );
-let timer_el = $( '#timer' );
+let target_el = $('#target');
+let timer_el = $('#timer');
 
-let overlay_el = $( '#overlay' );
-let start_el = $( '#start' );
-let leaderboard_el = $( '#leaderboard-list' );
-let history_el = $( '#history-list' );
+let overlay_el = $('#overlay');
+let start_el = $('#start');
+let leaderboard_el = $('#leaderboard-list');
+let history_el = $('#history-list');
 
-start_el.onmouseup = ( {button} ) => {
-    if( button !== 0 ) {
+start_el.onmouseup = ({ button }) => {
+    if (button !== 0) {
         return;
     }
 
-    if( game.start_enable && !game.running ) {
+    if (game.start_enable && !game.running) {
         start_game();
     }
 
-    if( !game.start_enable && !game.running ){
+    if (!game.start_enable && !game.running) {
         game.start_enable = true;
     }
 };
 
 target_el.onmousedown = () => {
-    if( game.running ) {
+    if (game.running) {
         end_game();
     }
 };
 
 target_el.onmouseup = () => {
-    if( !game.start_enable && !game.running ){
+    if (!game.start_enable && !game.running) {
         game.start_enable = true;
     }
 }
@@ -60,7 +60,7 @@ function start_game() {
     game.start_time = Date.now();
     game.react_time = Date.now() + (
         1000 +
-        Math.floor( Math.random() * 2000 )
+        Math.floor(Math.random() * 2000)
     );
 
     game.start_enable = false;
@@ -68,23 +68,23 @@ function start_game() {
 
 function end_game() {
     game.running = false;
-    if( game.reaction < 0 ) {
-        console.log( 'u suck' )
+    if (game.reaction < 0) {
+        console.log('u suck')
     }
 
-    if( game.reaction >= 0 ) {
+    if (game.reaction >= 0) {
         let score = {
             reaction: game.reaction,
             date: Date.now(),
             medal: game.reaction < 250,
         };
 
-        game.history.splice( 0, 0, score );
+        game.history.splice(0, 0, score);
         game.history = game.history.slice(0, 5);
 
-        game.leaderboard.push( score );
+        game.leaderboard.push(score);
         game.leaderboard = game.leaderboard
-            .sort( ( a, b ) => a.reaction - b.reaction )
+            .sort((a, b) => a.reaction - b.reaction)
             .slice(0, 5);
 
         game.dirty_boards = true;
@@ -94,34 +94,34 @@ function end_game() {
     overlay_el.style.opacity = 1;
 }
 
-function create_element( template, vars ) {
-    for( let name in vars ) {
-        template = template.replace( `{{${name}}}`, vars[name] );
+function create_element(template, vars) {
+    for (let name in vars) {
+        template = template.replace(`{{${name}}}`, vars[name]);
     }
 
-    template =  template
-        .replace( /{{(?:\s+)?if(?:\s+)?true(?:\s+)?{(.+)}(?:\s+)?}}/, '$1' )
-        .replace( /{{(?:\s+)?if(?:\s+)?.+(?:\s+)?{(.+)}(?:\s+)?}}/, '' );
+    template = template
+        .replace(/{{(?:\s+)?if(?:\s+)?true(?:\s+)?{(.+)}(?:\s+)?}}/, '$1')
+        .replace(/{{(?:\s+)?if(?:\s+)?.+(?:\s+)?{(.+)}(?:\s+)?}}/, '');
 
-    let div = document.createElement( 'div' );
+    let div = document.createElement('div');
     div.innerHTML = template;
     return div.children[0];
 }
 
 function draw() {
-    if( game.running ) {
+    if (game.running) {
         let position = Date.now() - game.react_time;
         game.reaction = position;
 
-        if( position < 0 ) {
+        if (position < 0) {
             timer_el.innerText = '';
             target_el.style.zIndex = 1;
             target_el.style.background = RED_COLOR;
             overlay_el.style.opacity = 0;
         }
 
-        if( position >= 0 ) {
-            if( target_el.style.background !== GREEN_COLOR ) {
+        if (position >= 0) {
+            if (target_el.style.background !== GREEN_COLOR) {
                 target_el.style.background = GREEN_COLOR;
             }
 
@@ -129,31 +129,31 @@ function draw() {
         }
     }
 
-    if( game.dirty_boards ) {
+    if (game.dirty_boards) {
         leaderboard_el.innerHTML = "";
         history_el.innerHTML = "";
 
-        for( let i = 0; i < 5; i++ ) {
+        for (let i = 0; i < 5; i++) {
             let leaderboard_score = game.leaderboard[i];
             let history_score = game.history[i];
 
-            if( typeof leaderboard_score !== 'undefined' ) {
-                leaderboard_el.appendChild( create_element( LEADERBOARD_TEMPLATE, leaderboard_score ) );
+            if (typeof leaderboard_score !== 'undefined') {
+                leaderboard_el.appendChild(create_element(LEADERBOARD_TEMPLATE, leaderboard_score));
             } else {
-                leaderboard_el.appendChild( document.createElement( 'li' ) );
+                leaderboard_el.appendChild(document.createElement('li'));
             }
 
-            if( typeof history_score !== 'undefined' ) {
-                history_el.appendChild( create_element( HISTORY_TEMPLATE, history_score ) );
+            if (typeof history_score !== 'undefined') {
+                history_el.appendChild(create_element(HISTORY_TEMPLATE, history_score));
             } else {
-                history_el.appendChild( document.createElement( 'li' ) );
+                history_el.appendChild(document.createElement('li'));
             }
         }
 
         game.dirty_boards = false;
     }
 
-    requestAnimationFrame( draw );
+    requestAnimationFrame(draw);
 }
 
 draw();
